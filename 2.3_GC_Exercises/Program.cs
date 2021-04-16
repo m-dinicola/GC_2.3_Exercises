751d2542a948b971b3936ec758496baf6811c594
mdinicola <the.m.dinicola@gmail.com>
2021-04-16 18:34:36 -0400

    commit 0

---
+++ 2.3_GC_Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._3_GC_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
